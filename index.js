b7f39d3545ddbfe60075256b8327627f2b71eff8
tigar <aatigar@gmail.com>
2020-01-08 22:45:34 -0800

    Adding helper functions and testing flags

--- index.js
+++ index.js
@@ -9,11 +9,28 @@ var prompt = require('co-prompt');
 
 
 program
+ .option('-l, --ls [n]', 'list n entries', parseInt)
  .arguments('<entry>')
  .parse(process.argv);
 
 var entry = program.args.join(' ');
-fs.appendFileSync('mrbub.txt', entry + '\n\n');
+if (program.ls) {
+    console.log("ls is working")
+}
+
+else {
+    fs.appendFileSync('mrbub.txt', timeStamp() + ' ' + entry + '\n\n');
+}
 console.log('Entry added');
 
 
+function timeStamp() {
+    var timestamp = new Date();
+    var now = timestamp.getFullYear() + '-' + addZero((timestamp.getMonth() + 1)) + '-' + addZero(timestamp.getDate()) + ' ' + addZero(timestamp.getHours()) + ':' + addZero(timestamp.getMinutes());
+    return now;
+  }
+
+function addZero(value) {
+    value = value < 10 ? '0' + value : value;
+    return value;
+}
